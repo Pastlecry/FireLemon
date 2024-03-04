69370adeff1d02e3568bb3aecdc016915417db8a
Amirreza Salahi <93829550+Pastlecry@users.noreply.github.com>
2024-03-04 12:52:24 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+class FireLemon():
+   __verrsion__ = "v2.0.0"
+
+"""  
+FireLemon is an advanced ddos tool powered by python's most powerful frameworks such as Scapy, requests, socket and socks
+Copyright 2024 Pastlecry     
+
+Github repository: https://github.com/Pastlecry/FireLemon
+"""
+
+if __name__ == "__main__":
+   import console
