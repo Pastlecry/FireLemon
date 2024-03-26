f97213ee24c3357e5bbb87b4867cdbe8fc337488
Amirreza Sl <93829550+Pastlecry@users.noreply.github.com>
2024-03-27 02:04:02 +0330

    Update host_handle.py

--- scripts/handle_attacks/host_handle.py
+++ scripts/handle_attacks/host_handle.py
@@ -158,7 +158,7 @@ def options(*args, **kwargs):
 
     session = PromptSession()
 
-    option =  session.prompt(*args, **kwargs, style=style, complete_in_thread=True, auto_suggest=AutoSuggestFromHistory())
+    option = session.prompt(*args, **kwargs, style=style, complete_in_thread=True, auto_suggest=AutoSuggestFromHistory())
 
     return option.strip()
     
